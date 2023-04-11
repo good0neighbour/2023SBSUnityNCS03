ce8f904736326ee110a5c0185c4ce412df2a770b
good0neighbour <100455847+good0neighbour@users.noreply.github.com>
2023-04-11 17:55:11 +0900

    2023.04.11: tilemap, CS_first
    Tilemap done.
    CSharp first start.

---
+++ cs_Random/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_Random
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Random tRandom = new Random(777);  //777을 씨앗으로 뿌림
+            Random tRandom = new Random();  //알아서 임의의 씨앗을 뿌림
+
+            int tDice = tRandom.Next();
+            Console.WriteLine($"Dice is {tDice.ToString()}");
+
+            tDice = tRandom.Next(1, 6 + 1); //[1, 6]
+            Console.WriteLine($"Dice is {tDice.ToString()}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ cs_first/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_first
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("cs_first");
+
+            int tA = 777;
+            Console.WriteLine("I am a Good Boy. This is {0}", tA);
+
+            float tB = 3.14159f;
+            Console.WriteLine("I am a Good Boy. This is {0:F2}", tB);   //F뒤에 붙은 숫자는 소수점 이하 몇 자리까지라는 의미
+
+            string tString = "string test";
+            Console.WriteLine("STRING is " + tString);
+
+            //개행
+            Console.WriteLine();
+
+            //보간 문자열
+            //보간 문자열 만드는 방법. $표시를 앞에 붙인다.
+            string tRyuString = $"We are The Good Boys. This is {tA}, PI is {tB}, STRING is {tString}";
+            Console.WriteLine(tRyuString);
+
+
+            Console.ReadLine();
+        }
+    }
+}
