241ccdef2e05c17e063fb0cdeb23b8d4b9689c8b
good0neighbour <notyeas@gmail.com>
2023-06-28 10:57:18 +0900

    2023.06.28 Camera rotation
    Camera looks at the actor.

--- gameEngineP_report/Assets/Scripts/CFollowCamera.cs
+++ gameEngineP_report/Assets/Scripts/CFollowCamera.cs
@@ -5,6 +5,7 @@ public class CFollowCamera : MonoBehaviour
     [SerializeField] private Vector3 mPositionOffset = Vector3.zero;
     [SerializeField] private Vector3 mLookAtOffset = Vector3.zero;
     [SerializeField] private Transform mActor = null;
+    private float mRaidanToDegree = 180.0f / Mathf.PI;
 
     private void Update()
     {
@@ -12,8 +13,24 @@ public class CFollowCamera : MonoBehaviour
         transform.localPosition = mActor.localPosition + mActor.rotation * mPositionOffset;
 
         // 카메라 회전
+        float tX;
+        float tY;
+        float tZ;
         Vector3 tGap = mActor.localPosition + mLookAtOffset - transform.localPosition;
-        Vector3 tRadian = new Vector3(Mathf.Atan(tGap.y / tGap.z), Mathf.Atan(tGap.z / tGap.x), Mathf.Atan(tGap.y / tGap.x));
-        transform.localRotation = Quaternion.Euler(tRadian / Mathf.PI * 180f);
+
+        if (0.0f > tGap.z)
+        {
+            tX = 180.0f - Mathf.Atan(tGap.y / tGap.z) * mRaidanToDegree;
+            tY = 180.0f + Mathf.Atan(tGap.x / tGap.z) * mRaidanToDegree;
+            tZ = 180.0f;
+        }
+        else
+        {
+            tX = -Mathf.Atan(tGap.y / tGap.z) * mRaidanToDegree;
+            tY = Mathf.Atan(tGap.x / tGap.z) * mRaidanToDegree;
+            tZ = 0.0f;
+        }
+
+        transform.localRotation = Quaternion.Euler(tX, tY, tZ);
     }
 }
