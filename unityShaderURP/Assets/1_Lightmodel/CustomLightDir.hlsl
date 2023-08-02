cf71b714c7c6303cabad60fcc0ca500045e537c7
good0neighbour <100455847+good0neighbour@users.noreply.github.com>
2023-08-02 17:51:36 +0900

    2023.08.02 URP BlinnPhong, Fresnel, Vertex, UV, Outline
    BlinnPhong
    Fresnel, RimLight
    Vertex animation
    UV animation
    Outline

--- unityShaderURP/Assets/1_Lightmodel/CustomLightDir.hlsl
+++ unityShaderURP/Assets/1_Lightmodel/CustomLightDir.hlsl
@@ -1,4 +1,10 @@
+//main light(장면에 배치된 direction light를 얻는다)의 빝벡터를 얻어오는 함수
 void CustomLightDir_float(out float3 tDir)
 {
-	float3 tDir = float3(1, 1, 1);
+#ifdef SHADERGRAPH_PREVIEW
+	tDir = float3(1, 1, 1);
+#else
+	Light tLight = GetMainLight();
+	tDir = tLight.direction;
+#endif
 }
